9c4e0a7f63b84dc404290a0545489843a0827cf5
justbaileym <justbaileym@users.noreply.github.com>
2024-10-08 09:10:50 +0200

    WIP: tracing example

--- server-example/src/main.rs
+++ server-example/src/main.rs
@@ -16,10 +16,91 @@ use dropshot::TypedBody;
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
+use snafu::{ResultExt, Snafu};
+use std::error::Error;
 use std::io::Write;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering;
 use tempfile::NamedTempFile;
+use thiserror::Error;
+use tracing::info;
+use tracing::{debug, error, info, span, trace, warn, Level};
+use tracing_subscriber;
+
+// the `#[tracing::instrument]` attribute creates and enters a span
+// every time the instrumented function is called. The span is named after the
+// the function or method. Paramaters passed to the function are recorded as fields.
+#[tracing::instrument]
+pub fn shave(yak: usize) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    // this creates an event at the TRACE log level with two fields:
+    // - `excitement`, with the key "excitement" and the value "yay!"
+    // - `message`, with the key "message" and the value "hello! I'm gonna shave a yak."
+    //
+    // unlike other fields, `message`'s shorthand initialization is just the string itself.
+    trace!(excitement = "yay!", "hello! I'm gonna shave a yak");
+    if yak == 3 {
+        warn!("could not locate yak");
+        return OutOfSpaceError
+            .fail()
+            .map_err(|source| MissingYakError::OutOfSpace { source })
+            .context(MissingYak)
+            .map_err(|err| err.into());
+    } else {
+        trace!("yak shaved successfully");
+    }
+    Ok(())
+}
+
+pub fn shave_all(yaks: usize) -> usize {
+    // Constructs a new span named "shaving_yaks" at the INFO level,
+    // and a field whose key is "yaks". This is equivalent to writing:
+    //
+    // let span = span!(Level::INFO, "shaving_yaks", yaks = yaks);
+    //
+    // local variables (`yaks`) can be used as field values
+    // without an assignment, similar to struct initializers.
+    let span = span!(Level::INFO, "shaving_yaks", yaks);
+    let _enter = span.enter();
+
+    info!("shaving yaks");
+
+    let mut yaks_shaved = 0;
+    for yak in 1..=yaks {
+        let res = shave(yak);
+        debug!(target: "yak_events", yak, shaved = res.is_ok());
+
+        if let Err(ref error) = res {
+            // Like spans, events can also use the field initialization shorthand.
+            // In this instance, `yak` is the field being initalized.
+            error!(yak, error = error.as_ref(), "failed to shave yak");
+        } else {
+            yaks_shaved += 1;
+        }
+        trace!(yaks_shaved);
+    }
+
+    yaks_shaved
+}
+
+// Error types
+// Usually you would pick one error handling library to use, but they can be mixed freely
+#[derive(Debug, Snafu)]
+enum OutOfSpaceError {
+    #[snafu(display("out of cash"))]
+    OutOfCash,
+}
+
+#[derive(Debug, Error)]
+enum MissingYakError {
+    #[error("out of space")]
+    OutOfSpace { source: OutOfSpaceError },
+}
+
+#[derive(Debug, Snafu)]
+enum YakError {
+    #[snafu(display("missing yak"))]
+    MissingYak { source: MissingYakError },
+}
 
 // This function would not be used in a normal application. It is used to
 // generate temporary keys and certificates for the purpose of this demo.
@@ -49,6 +130,18 @@ fn generate_keys() -> Result<(NamedTempFile, NamedTempFile), String> {
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
+    // install global subscriber configured based on RUST_LOG envvar.
+    tracing_subscriber::fmt::init();
+
+    let number_of_yaks = 3;
+    // this creates a new event, outside of any spans.
+    info!(number_of_yaks, "preparing to shave yaks");
+
+    let number_shaved = yak_shave::shave_all(number_of_yaks);
+    info!(
+        all_yaks_shaved = number_shaved == number_of_yaks,
+        "yak shaving completed."
+    );
     // Begin by generating TLS certificates and keys and stuffing them into a
     // TLS configuration.
     let (cert_file, key_file) = generate_keys()?;
